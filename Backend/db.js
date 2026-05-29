ae519ecde281157de5e5f07d2867b8bb324a4359
Dilip <dilip@example.com>
2026-05-29 15:35:47 +0530

    Fix SSL issuer error by using native Node root CAs

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,9 @@
 import pg from "pg";
 import dotenv from "dotenv";
-import fs from "fs";
-import Path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = Path.dirname(__filename);
 
 dotenv.config();
 
-const db= new pg.Pool({
+const db = new pg.Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
@@ -17,8 +11,7 @@ const db= new pg.Pool({
     port: process.env.PG_PORT,    
     ssl: {
         rejectUnauthorized: true,
-        ca: fs.readFileSync(Path.join(__dirname, "isrgrootx1.pem")).toString(),
     },
-})
+});
 
 export default db;
